49b3d9b033ddcacaf78e9fa7361f384af78d0fbd
Sfernandez <sergio_2297@hotmail.es>
2024-07-30 21:48:48 +0200

    feat: adds HexCode colour codification

---
+++ src/main/java/es/sfernandez/color/model/HexCode.java
@@ -0,0 +1,48 @@
+package es.sfernandez.color.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class HexCode
+        implements AcceptedByCssColourCode {
+
+    //---- Constants and Definitions ----
+    public static final Pattern hexCodePattern = Pattern.compile("#?([0-9a-fA-F]{8}|[0-9a-fA-F]{6}|[0-9a-fA-F]{4}|[0-9a-fA-F]{3})");
+
+    //---- Attributes ----
+    private final String value;
+
+    //---- Constructor ----
+    public HexCode(final String code) {
+        if(code == null || code.isBlank())
+            throw new IllegalArgumentException("Can not create an HexCode with null/blank value.");
+
+        this.value = inferValueFrom(code);
+    }
+
+    private String inferValueFrom(String code) {
+        Matcher matcher = matchHexCode(code);
+
+        return "#" + matcher.group(1).toUpperCase();
+    }
+
+    private Matcher matchHexCode(String code) {
+        Matcher matcher = hexCodePattern.matcher(code);
+
+        if(!matcher.matches())
+            throw new IllegalArgumentException("Given expression doesn't match an HexCode. (value='" + code + "')");
+
+        return matcher;
+    }
+
+    //---- Methods ----
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String toCssCode() {
+        return value;
+    }
+
+}
---
+++ src/test/java/es/sfernandez/color/model/HexCodeTest.java
@@ -0,0 +1,71 @@
+package es.sfernandez.color.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class HexCodeTest {
+
+    @Test
+    void createWith_nullCode_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HexCode(null));
+    }
+
+    @Test
+    void createWith_emptyCode_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HexCode("   "));
+    }
+
+    @Test
+    void createFromIncorrectCode_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HexCode("#HAW345"));
+        assertThrows(IllegalArgumentException.class, () -> new HexCode("#12345"));
+        assertThrows(IllegalArgumentException.class, () -> new HexCode("2"));
+        assertThrows(IllegalArgumentException.class, () -> new HexCode("#12"));
+        assertThrows(IllegalArgumentException.class, () -> new HexCode("#FFAABBEE1"));
+    }
+
+    @Test
+    void createFromValidCode_worksTest() {
+        String code = "#AE12FA";
+        HexCode hexCode = new HexCode(code);
+
+        assertThat(hexCode.value()).isEqualTo(code);
+    }
+
+    @Test
+    void createFromValidCode_withoutHashtag_worksTest() {
+        String code = "AE12FA";
+        HexCode hexCode = new HexCode(code);
+
+        assertThat(hexCode.value()).isEqualTo("#" + code);
+    }
+
+    @Test
+    void createWithOpacity_worksTest() {
+        String code = "#B1C2D3FE";
+        HexCode hexCode = new HexCode(code);
+
+        assertThat(hexCode.value()).isEqualTo("#B1C2D3FE");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"#fff", "#123456", "123", "#aaff", "#AaBbCcDd"})
+    void cssCodeIsEqualToValueTest(String cssCode) {
+        HexCode hexCode = new HexCode(cssCode);
+
+        assertThat(hexCode.value()).isEqualTo(hexCode.toCssCode());
+    }
+
+    @Test
+    void codeIsAlwaysUpperCasedTest() {
+        String code = "#b1c2D0fE";
+        HexCode hexCode = new HexCode(code);
+
+        assertThat(hexCode.value()).isEqualTo("#B1C2D0FE");
+    }
+
+}
