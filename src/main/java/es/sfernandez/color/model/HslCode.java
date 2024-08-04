c12467eb594c2abdd01a23823bfa36a5029d9118
Sfernandez <sergio_2297@hotmail.es>
2024-08-04 19:32:49 +0200

    feat: adds HslCode colour codification

---
+++ src/main/java/es/sfernandez/color/model/HslCode.java
@@ -0,0 +1,105 @@
+package es.sfernandez.color.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public record HslCode(float hue, float saturation, float lightness)
+        implements AcceptedByCssColourCode {
+
+    //---- Constants and Definitions ----
+    public static final Pattern cssCodePattern = Pattern.compile("hsl\\(\\s*(\\d{1,3})\\s*,\\s*(\\d{1,3})%\\s*,\\s*(\\d{1,3})%\\s*\\)");
+
+    //---- Constructor ----
+    public HslCode {
+        assertIsValid(hue, "Red");
+        assertIsValid(saturation, "Green");
+        assertIsValid(lightness, "Blue");
+    }
+
+    public HslCode(int hue, int saturation, int lightness) {
+        this(normalizeDegrees(hue, "Hue"), normalizePercentage(saturation, "Saturation"), normalizePercentage(lightness, "Lightness"));
+    }
+
+    public HslCode(String cssCode) {
+        this(extractHueValueFromCssCode(cssCode), extractSaturationValueFromCssCode(cssCode), extractLightnessValueFromCssCode(cssCode));
+    }
+
+    private static int extractHueValueFromCssCode(String cssCode) {
+        return Integer.parseInt(matchCssCode(cssCode).group(1));
+    }
+
+    private static int extractSaturationValueFromCssCode(String cssCode) {
+        return Integer.parseInt(matchCssCode(cssCode).group(2));
+    }
+
+    private static int extractLightnessValueFromCssCode(String cssCode) {
+        return Integer.parseInt(matchCssCode(cssCode).group(3));
+    }
+
+    private static Matcher matchCssCode(String cssCode) {
+        Matcher matcher = cssCodePattern.matcher(cssCode);
+
+        if(!matcher.matches())
+            throw new IllegalArgumentException("Given expression doesn't match CSS hsl code. (value='" + cssCode + "')");
+
+        return matcher;
+    }
+
+    private static void assertIsValid(float value, String propertyName) {
+        if(isNotBetween0And1(value))
+            throw new IllegalArgumentException(propertyName + " value is out of range [0.0, 1.0]. (value=" + value + ")");
+    }
+
+    private static boolean isNotBetween0And1(float value) {
+        return value < 0.0f || value > 1.0f;
+    }
+
+    private static float normalizeDegrees(int value, String propertyName) {
+        if(isNotBetween0And360(value))
+            throw new IllegalArgumentException(propertyName + " value is out of range [0, 360]. (value=" + value + ")");
+
+        return value / 360.0f;
+    }
+
+    private static boolean isNotBetween0And360(int value) {
+        return value < 0 || value > 360;
+    }
+
+    private static float normalizePercentage(int value, String propertyName) {
+        if(isNotBetween0And100(value))
+            throw new IllegalArgumentException(propertyName + " value is out of range [0, 100]. (value=" + value + ")");
+
+        return value / 100.0f;
+    }
+
+    private static boolean isNotBetween0And100(int value) {
+        return value < 0 || value > 100;
+    }
+
+    //---- Methods ----
+    @Override
+    public String toCssCode() {
+        return String.format("hsl(%d, %d%%, %d%%)", hueDegrees(), saturationPercentage(), lightnessPercentage());
+    }
+
+    public int hueDegrees() {
+        return normalizeToDegrees(hue);
+    }
+
+    public int saturationPercentage() {
+        return normalizeToPercentage(saturation);
+    }
+
+    public int lightnessPercentage() {
+        return normalizeToPercentage(lightness);
+    }
+
+    private int normalizeToDegrees(final float value) {
+        return (int) (value * 360);
+    }
+
+    private int normalizeToPercentage(final float value) {
+        return (int) (value * 100);
+    }
+
+}
---
+++ src/test/java/es/sfernandez/color/model/HslCodeTest.java
@@ -0,0 +1,150 @@
+package es.sfernandez.color.model;
+
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HslCodeTest {
+
+    @Test
+    void createWith_negativeFloatHueValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(-1f, 0f, 0f));
+    }
+
+    @Test
+    void createWith_negativeFloatSaturationValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0f, -1.0f, 0f));
+    }
+
+    @Test
+    void createWith_negativeFloatLightnessValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0f, 0f, -1f));
+    }
+
+    @Test
+    void createWith_biggerThanOneHueValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(1.1f, 0f, 0f));
+    }
+
+    @Test
+    void createWith_biggerThanOneSaturationValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0f, 1.1f, 0f));
+    }
+
+    @Test
+    void createWith_biggerThanOneLightnessValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0f, 0f, 1.1f));
+    }
+
+    @Test
+    void createWith_negativeIntHueValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(-1, 0, 0));
+    }
+
+    @Test
+    void createWith_negativeIntSaturationValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0, -1, 0));
+    }
+
+    @Test
+    void createWith_negativeIntLightnessValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0, 0, -1));
+    }
+
+    @Test
+    void createWith_biggerThan360HueValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(361, 0, 0));
+    }
+
+    @Test
+    void createWith_biggerThan100SaturationValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0, 101, 0));
+    }
+
+    @Test
+    void createWith_biggerThan100LightnessValue_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode(0, 0, 101));
+    }
+
+    @Test
+    void hueValue_createdFromDegrees_isCorrectlyNormalizedTest() {
+        HslCode hsl = new HslCode(90, 0, 0);
+
+        assertThat(hsl.hue()).isCloseTo(0.25f, Offset.offset(0.005f));
+    }
+
+    @Test
+    void saturationValue_createdFromPercentage_isCorrectlyNormalizedTest() {
+        HslCode hsl = new HslCode(0, 50, 0);
+
+        assertThat(hsl.saturation()).isCloseTo(0.5f, Offset.offset(0.005f));
+    }
+
+    @Test
+    void lightnessValue_createdFromPercentage_isCorrectlyNormalizedTest() {
+        HslCode hsl = new HslCode(0, 0, 75);
+
+        assertThat(hsl.lightness()).isCloseTo(0.75f, Offset.offset(0.005f));
+    }
+
+    @Test
+    void hueValue_createdFromNormalizedValue_isCorrectlyNormalizedToDegreesTest() {
+        HslCode rgb = new HslCode(0.75f, 0, 0);
+
+        assertThat(rgb.hueDegrees()).isEqualTo(270);
+    }
+
+    @Test
+    void saturationValue_createdFromNormalizedValue_isCorrectlyNormalizedToPercentageTest() {
+        HslCode rgb = new HslCode(0, 0.5f, 0);
+
+        assertThat(rgb.saturationPercentage()).isEqualTo(50);
+    }
+
+    @Test
+    void lightnessValue_createdFromNormalizedValue_isCorrectlyNormalizedToPercentageTest() {
+        HslCode rgb = new HslCode(0, 0, 0.75f);
+
+        assertThat(rgb.lightnessPercentage()).isEqualTo(75);
+    }
+
+    @Test
+    void cssCode_isCorrectlyGenerated_fromNormalizedCodeTest() {
+        HslCode rgb = new HslCode(0.25f, 0.5f, 0.75f);
+
+        assertThat(rgb.toCssCode()).isEqualTo("hsl(90, 50%, 75%)");
+    }
+
+    @Test
+    void cssCode_isCorrectlyGenerated_fromNormalized255CodeTest() {
+        HslCode rgb = new HslCode(230, 15, 88);
+
+        assertThat(rgb.toCssCode()).isEqualTo("hsl(230, 15%, 88%)");
+    }
+
+    @Test
+    void createFromIncorrectCssCode_throwsIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new HslCode("hls(0,0,0)"));
+        assertThrows(IllegalArgumentException.class, () -> new HslCode("hsl[0,0,0]"));
+        assertThrows(IllegalArgumentException.class, () -> new HslCode("hsl(0)"));
+        assertThrows(IllegalArgumentException.class, () -> new HslCode("hsla(0,0,0,0)"));
+        assertThrows(IllegalArgumentException.class, () -> new HslCode("hsl(-1,0,0)"));
+        assertThrows(IllegalArgumentException.class, () -> new HslCode("hsl(0,0.5,0)"));
+        assertThrows(IllegalArgumentException.class, () -> new HslCode("hsl(90,0%,45)"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"hsl(63, 27%, 88%)", "hsl(63   , 27%, 88%    )", "hsl(63,27%,88%)"})
+    void createFromCssCode_worksTest(String cssCode) {
+        HslCode rgb = new HslCode(cssCode);
+
+        assertThat(rgb.hueDegrees()).isEqualTo(63);
+        assertThat(rgb.saturationPercentage()).isEqualTo(27);
+        assertThat(rgb.lightnessPercentage()).isEqualTo(88);
+    }
+
+}
